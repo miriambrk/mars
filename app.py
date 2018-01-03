169cf827c25cbae56babbadc1d40a8cbf5c403ae
Miriam A Berkowitz <miriambrk@gmail.com>
2018-01-03 18:06:59 -0500

    another try

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ def scrape():
         upsert=True
     )
     #######return redirect("http://localhost:5000/", code=302)
-    return redirect("mars-mission.herokuapp.com", code=302)
+    print("after scrape; ready to redirect")
+    return redirect("https://mars-mission.herokuapp.com", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
